4af8b239ec5111e00479236ec34067c17bf5bbca
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-22 15:06:02 +0200

    impostazione logica

---
+++ main.js
@@ -0,0 +1,21 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, con le seguenti regole:
+// al posto dei multipli di 3 stampi "Fizz"
+// al posto dei multipli di 5 stampi "Buzz"
+// al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
+
+// creare un ciclo for di numeri da 1 a 100
+
+// creare un array collegato al ciclo
+
+// stampare gli elementi da 1 a 100
+
+// se numero divisibile per 3 scrivi Fizz
+// il numero diviso 3 deve dare resto 0
+
+// se numero divisibile per 5 scrivi Buzz
+// il numero diviso 5 deve dare resto 0
+
+// se numero divisibile per 3 e per 5 scrivi FizzBuzz
+// sono tutti i numeri divisibili per 15
+
+// stampa in pagina
